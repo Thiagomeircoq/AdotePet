8d09c7391588bcd45da49d2d86a1e28ccfc90534
Thiago Meira <=>
2024-10-13 18:53:19 -0300

    Adicionando a logo na página de Login

--- pages/login.vue
+++ pages/login.vue
@@ -24,23 +24,20 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
         <div class="flex h-screen">
             <div class="w-1/2 py-20 px-20 flex flex-col items-center justify-center">
                 <div class="w-2/4">
+                    <img width="250px" src="../public/img/adotepet-logo.svg" alt="" srcset="">
                     <div class="flex flex-col items-center">
-                        <img width="200px" src="../public/img/adotepet-logo.svg" alt="" srcset="">
                         <div class="text-center mt-3">
-                            <h1 class="text-amber-500 font-semibold text-lg">Faça seu login</h1>
-                            <p class="text-stone-500 text-sm">
-                                Para divulgar ou adotar um animalzinho, você precisa ter um cadastro
-                            </p>
+                            <h1 class="text-stone-700 font-semibold text-xl">Faça o login</h1>
                         </div>
                     </div>
                     <UForm :schema="schema" :state="state" class="mt-12 flex flex-col gap-5" @submit="onSubmit">
                         <UFormGroup label="E-mail" name="email">
-                            <UInput icon="i-heroicons-envelope" size="lg" v-model="state.email" color="primary"
-                                type="email" variant="outline" placeholder="email@exemplo.com" />
+                            <UInput icon="i-heroicons-envelope" size="lg" v-model="state.email" type="email"
+                                variant="outline" placeholder="email@exemplo.com" />
                         </UFormGroup>
                         <UFormGroup label="Senha" name="password">
-                            <UInput icon="i-heroicons-lock-closed" size="lg" v-model="state.password" color="primary"
-                                type="password" variant="outline" placeholder="Senha" />
+                            <UInput icon="i-heroicons-lock-closed" size="lg" v-model="state.password" type="password"
+                                variant="outline" placeholder="Senha" />
                         </UFormGroup>
                         <UButton class="flex justify-center" size="lg" color="amber" variant="solid" trailing>
                             Entrar
@@ -48,8 +45,10 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
                     </UForm>
                 </div>
             </div>
-            <div class="w-1/2 h-full">
-                <img class="w-full" src="https://via.placeholder.com/150" alt="" srcset="">
+            <div class="w-1/2 bg-stone-200">
+                <div class="flex h-full items-center justify-center">
+                    <img class="w-8/12" src="../public/img/adote-nos.svg" alt="" srcset="">
+                </div>
             </div>
         </div>
     </div>
