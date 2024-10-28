8fa5a77ad9ba33b786ba3e49f3be94774c02499a
Thiago Meira <thiago5meira@outlook.com>
2024-10-27 22:05:46 -0300

    Criando a funcionalidade de Logout e criando uma nova página de Anunciar um PET

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,31 +2,6 @@
 import { storeToRefs } from 'pinia';
 import { useAuthStore } from '~/store/auth';
 
-const links = [
-    [
-        {
-            label: 'Busca',
-            to: '',
-            dropdown: true,
-        },
-        {
-            label: 'Adoção',
-            to: '',
-            dropdown: true,
-        },
-        {
-            label: 'Conheça',
-            to: '',
-            dropdown: true,
-        },
-        {
-            label: 'Ajude',
-            to: '',
-            dropdown: true,
-        },
-    ]
-]
-
 const authStore = useAuthStore();
 const { authenticated } = storeToRefs(authStore);
 </script>
@@ -34,15 +9,14 @@ const { authenticated } = storeToRefs(authStore);
 <template>
     <header class="container mx-auto flex items-center justify-between p-4">
         <div class="flex items-center justify-center">
-            <a href="#">
-                <img width="200px" src="../public/img/adotepet-logo.svg" alt="" srcset="">
-            </a>
+            <img class="cursor-pointer" width="200px" @click="navigateTo('/')" src="../public/img/adotepet-logo.svg" alt="" srcset="">
         </div>
 
         <div class="flex items-center space-x-4">
-            <UButton size="md" color="amber" variant="solid" label="Anunciar" trailing />
-            <UButton v-if="!authenticated" size="md" color="amber" variant="outline" label="Entrar" trailing />
-            
+            <UButton size="md" color="amber" @click="navigateTo('/anunciar/pet-para-adocao')" variant="solid" label="Anunciar" trailing />
+            <UButton v-if="!authenticated" @click="navigateTo('/login')" size="md" color="amber" variant="outline"
+                label="Entrar" trailing />
+
             <template v-else>
                 <div class="flex gap-2">
                     <div class="border border-amber-500 p-1 rounded-full flex justify-center items-center">
@@ -50,7 +24,7 @@ const { authenticated } = storeToRefs(authStore);
                     </div>
                     <div class="flex flex-col justify-center">
                         <span class="text-xs font-semibold text-gray-800">Thiago Meira</span>
-                        <a class="text-xs text-gray-800 underline" href="#">Sair</a>
+                        <a class="text-xs text-gray-800 underline" @click="authStore.logUserOut" href="#">Sair</a>
                     </div>
                 </div>
             </template>
